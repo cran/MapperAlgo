9ff4687bec422b8ff03e9f84f045624e1d48fcaf
ChiChien Wang <kennywang2003@gmail.com>
2024-12-08 15:40:02 +0000

    version 1.0.1

---
+++ R/Cluster.R
@@ -0,0 +1,161 @@
+#' Perform clustering within a level set
+#'
+#' @param points_in_this_level Points in the current level set.
+#' @param filter_values The filter values.
+#' @param num_bins_when_clustering Number of bins when clustering.
+#' @param methods Specify the clustering method to be used, e.g., "hclust" or "kmeans".
+#' @param method_params A list of parameters for the clustering method.
+#' @return A list containing the number of vertices, external indices, and internal indices.
+#' @importFrom stats as.dist hclust cutree dist kmeans
+#' @export
+perform_clustering <- function(
+    points_in_this_level, 
+    filter_values, 
+    num_bins_when_clustering, 
+    methods, 
+    method_params = list()
+    ) {
+  num_points_in_this_level <- length(points_in_this_level)
+  
+  if (num_points_in_this_level == 0) {
+    return(list(num_vertices = 0, external_indices = NULL, internal_indices = NULL))
+  }
+  
+  if (num_points_in_this_level == 1) {
+    return(list(num_vertices = 1, external_indices = points_in_this_level, internal_indices = c(1)))
+  }
+
+  level_dist_object <- as.dist(as.matrix(dist(filter_values))[points_in_this_level, points_in_this_level])
+    
+    clustering_methods <- list(
+      hierarchical = function() {
+        level_max_dist <- max(level_dist_object)
+        level_hclust <- hclust(level_dist_object, method = "single")
+        level_heights <- level_hclust$height
+        level_cutoff <- cluster_cutoff_at_first_empty_bin(level_heights, level_max_dist, num_bins_when_clustering)
+        level_external_indices <- points_in_this_level[level_hclust$order]
+        level_internal_indices <- as.vector(cutree(list(
+          merge = level_hclust$merge,
+          height = level_hclust$height,
+          labels = level_external_indices), h = level_cutoff))
+        num_vertices_in_this_level <- max(level_internal_indices)
+        list(level_external_indices, level_internal_indices, num_vertices_in_this_level)
+      },
+      kmeans = function() {
+        max_clusters <- min(method_params$max_kmeans_clusters, num_points_in_this_level)
+        level_filter_values <- filter_values[points_in_this_level, , drop = FALSE]
+        if (max_clusters < nrow(level_filter_values)) {
+          level_kmean <- kmeans(level_filter_values, centers = max_clusters)
+          list(
+            points_in_this_level[order(level_kmean$cluster)], 
+            as.vector(level_kmean$cluster), 
+            max(level_kmean$cluster)
+          )
+        } else {
+          list(points_in_this_level, rep(1, num_points_in_this_level), 1)
+        }
+      },
+      dbscan = function() {
+        level_filter_values <- filter_values[points_in_this_level, , drop = FALSE]
+        dbscan_result <- dbscan::dbscan(
+          level_filter_values, 
+          eps = method_params$eps, 
+          minPts = method_params$minPts
+        )
+        if (max(dbscan_result$cluster) > 0) {
+          list(
+            points_in_this_level[order(dbscan_result$cluster)], 
+            as.vector(dbscan_result$cluster), 
+            max(dbscan_result$cluster)
+          )
+        } else {
+          list(points_in_this_level, rep(1, num_points_in_this_level), 1)
+        }
+      },
+      pam = function() {
+        level_filter_values <- filter_values[points_in_this_level, , drop = FALSE]
+        if (nrow(level_filter_values) >= 2) {
+          num_clusters <- min(method_params$num_clusters, nrow(level_filter_values) - 1)
+          pam_result <- cluster::pam(level_filter_values, k = num_clusters)
+          if (max(pam_result$clustering) > 0) {
+            list(
+              points_in_this_level[order(pam_result$clustering)], 
+              as.vector(pam_result$clustering), 
+              max(pam_result$clustering)
+            )
+          } else {
+            list(points_in_this_level, rep(1, num_points_in_this_level), 1)
+          }
+        } else {
+          list(points_in_this_level, rep(1, num_points_in_this_level), 1)
+        }
+      }
+    )
+    
+    if (!methods %in% names(clustering_methods)) {
+      stop("Invalid method provided")
+    }
+    clustering_result <- clustering_methods[[methods]]()
+
+  return(list(
+    num_vertices = clustering_result[[3]],
+    external_indices = clustering_result[[1]],
+    internal_indices = clustering_result[[2]]
+  ))
+}
+
+#' Cut the hierarchical clustering tree to define clusters
+#'
+#' @param heights Heights of the clusters.
+#' @param diam Diameter of the clusters.
+#' @param num_bins_when_clustering Number of bins when clustering.
+#' @return The cutoff height for the clusters.
+#' @importFrom graphics hist
+#' @export
+cluster_cutoff_at_first_empty_bin <- function(heights, diam, num_bins_when_clustering) {
+  if (length(heights) == 1) {
+    if (heights == diam) {
+      cutoff <- Inf
+    }
+  }
+  bin_breaks <- seq(from=min(heights), to=diam, by=(diam - min(heights))/num_bins_when_clustering)
+  
+  if (length(bin_breaks) == 1) { bin_breaks <- 1 }
+  
+  myhist <- hist(c(heights,diam), breaks=bin_breaks, plot=FALSE)
+  z <- (myhist$counts == 0)
+  if (sum(z) == 0) {
+    cutoff <- Inf
+  } else {
+    cutoff <- myhist$mids[ min(which(z == TRUE)) ]
+  }
+  return(cutoff)
+}
+
+#' Find the optimal number of clusters for k-means
+#'
+#' This function calculates the total within-cluster sum of squares (WSS) for a range
+#' of cluster numbers and identifies the best number of clusters (k) based on the 
+#' elbow method.
+#'
+#' @param dist_object A distance matrix or data frame containing the data to be clustered.
+#' @param max_clusters The maximum number of clusters to test for k-means. Default is 10.
+#' @return The optimal number of clusters (k) based on the elbow method.
+#' @importFrom stats kmeans
+#' @export
+find_best_k_for_kmeans <- function(dist_object, max_clusters = 10) {
+  # elbow method
+  wss_values <- numeric(max_clusters)
+  
+  for (k in 1:max_clusters) {
+    kmean_result <- kmeans(dist_object, centers = k, nstart = 25)  # nstart for more stable results
+    wss_values[k] <- kmean_result$tot.withinss  # Total within-cluster sum of squares
+  }
+  
+  differences <- diff(wss_values)
+  second_differences <- diff(differences)
+  
+  best_k <- which(second_differences == min(second_differences)) + 1
+  
+  return(best_k)
+}
--- README.md
+++ README.md
@@ -2,15 +2,64 @@
 
 This package is based on the `TDAmapper` package by Paul Pearson. You can view the original package [here](https://github.com/paultpearson/TDAmapper). Since the original package hasn't been updated in over seven years, this version is focused on optimization. By incorporating vector computation into the Mapper algorithm, this package aims to significantly improve its performance.
 
+## Get started quickly
+
+![Mapper](man/figures/mapper.png) Step visualize from [Skaf et al.](https://doi.org/10.1016/j.jbi.2022.104082)
+
+**Mapper is basically a three-step process:**
+
+1\. **Cover**: This step splits the data into overlapping intervals and creates a cover for the data.
+
+2\. **Cluster**: This step clusters the data points in each interval the cover creates.
+
+3\. **Simplicial Complex**: This step combines the two steps above, which connects the data points in the cover to create a simplicial complex.
+
+> you can know more about the basic here: Chazal, F., & Michel, B. (2021). An introduction to topological data analysis: fundamental and practical aspects for data scientists. Frontiers in artificial intelligence, 4, 667963.
+
+**Besides to the steps above, you can find the following code in the package:**
+
+1.  Mapper.R: Combining the three steps above
+2.  ConvertLevelset.R: Converting a Flat Index to a Multi-index, or vice versa.
+3.  EdgeVertices.R This is to find the nodes for plot, not for the Mapper algorithm.
+
 ## Goals
 
 Although this project serves as a personal training exercise, I have set several key objectives:
 
 1.  **Optimization**: While the current version speeds up computations by 100 times as the dataset grows, there are still some computational challenges that need to be addressed.
 
-2.  **Expanded Clustering Methods**: Clustering is a crucial component of the Mapper algorithm. In addition to hierarchical clustering, I aim to include a variety of clustering techniques to increase flexibility and adaptability.
+2.  **Expanded Clustering Methods**: Clustering is a crucial component of the Mapper algorithm. In addition to hierarchical clustering, I aim to include a variety of clustering techniques to increase flexibility and adaptability. (ex. K-means, DBSCAN, etc.)
 
 3.  **Code Structure**: The code is still under development and may be challenging to understand in its current form. My goal is to streamline the structure and provide a simple white paper that explains how to use the method effectively.
+
+4.  **Interval Clustering Optimization**: Most of the packages haven't produced the best clustering results in each interval. However, if this could be incorporated while computing the Mapper, it would be powerful. This will be part of future work, although it might increase computational complexity.
+
+5.  **Parallel**: Given the problem mentioned above, I plan to add parallel computation in the future. This will allow for flexible adjustment of the number of cores used for parallel computation, making the process more efficient.
+
+### Example
+
+``` r
+Mapper <- MapperAlgo(
+  # filter_values = iris[,1:4],
+  filter_values = circle_data[,2:3],
+  intervals = 4,
+  percent_overlap = 50,
+  num_bins_when_clustering = 10, 
+  methods = "dbscan", 
+  method_params = list(eps = 0.5, minPts = 5)
+  )
+```
+
+<table>
+  <tr>
+    <td><img src="man/figures/Circle.png" alt="Circle" width="500"/></td>
+    <td><img src="man/figures/CircleMapper.png" alt="CircleMapper" width="300"/></td>
+  </tr>
+</table>
+
+### Update Feature
+
+Version 1.1: <br/>1. Update more readable structure <br/>2. Added Kmeans, DBscan, and PAM clustering methods for user
 
 ## Why R?
 
@@ -21,8 +70,11 @@ While many TDA methods in Python have fewer computational limitations, there is 
 I've written some articles on Medium, which you can find [here](https://medium.com/@kennywang2003) to get familiar with topological data analysis. I'll be continuously updating my work, and I welcome any feedback!
 
 ### Build And Submit:
+
 This is for the author to submit the package to CRAN.
+
 ``` r
+devtools::document()
 devtools::build()
 devtools::submit_cran()
 ```
